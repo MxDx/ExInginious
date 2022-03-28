09dc8a7f9f3c289cceb7699e9eceec4959912eed
Maxime Delacroix <maxime.dxl2002@gmail.com>
2022-03-28 21:23:03 +0200

    set and get commit

--- Semaine5/fileManagement.c
+++ Semaine5/fileManagement.c
@@ -2,6 +2,8 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/mman.h>
 #include <string.h>
 
@@ -105,6 +107,78 @@ int sum_file(char *filename) {
     return rep;
 }
 
+/*
+ * @pre filename != NULL, index >= 0
+ * @post return the integer at the index {index}
+ *       of the array in the file {filename}.
+ *       return -1 in case of error.
+ *       return -2 if index >= length of array.
+ */
+int get(char *filename, int index) {
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) return -1;
+
+    struct stat fst;
+    int fstRep = fstat(fd, &fst);
+    if (fstRep == -1) return -1; 
+
+    int size = fst.st_size;
+    if (size/sizeof(int) <= index) {
+        int fc = close(fd);
+        if (fc == -1) return -1;
+        return -2;
+    }
+
+    int* mem = mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0);
+    if (mem == MAP_FAILED) return -1;
+
+    int rep = mem[index];
+
+    int unmap = munmap(mem, size);
+    if (unmap == -1) return -1;
+
+    int fc = close(fd);
+    if (fc == -1) return -1;
+
+    return rep;
+}
+
+/*
+ * @pre filename != NULL, index >= 0
+ * @post set the element in the file {filename}
+ *       at index {index} at value {value}.
+ *       do nothing in case of errors
+ */
+void set(char *filename, int index, int value) {
+    struct stat buf;
+    int fd  =  open (filename , O_RDWR | O_CREAT ) ;
+    if ( fd == -1) {
+        return;
+    }
+    int size = fstat( fd , &buf ) ; 
+    if ( size == -1){
+       return ; 
+    }
+    size_t len = buf.st_size/sizeof(int);
+    if ( index >= len ){ 
+        close(fd);
+        return ;
+    }
+    int *mem  = mmap (NULL, len*sizeof(int), PROT_WRITE, MAP_SHARED, fd, 0) ; 
+    if ( mem == MAP_FAILED ){
+        return ; 
+    }
+    mem[index] = value ;   
+    
+    int unmem =  munmap( mem , len ) ; 
+    if ( unmem == -1 ){
+        return;
+    }
+    int fdc = close( fd) ;
+    if ( fdc  == -1){
+        return ;
+    }
+}
 
 int main() {
     char* filename = "FileExists.exe";
@@ -119,4 +193,5 @@ int main() {
     int size = 10;
     printf("%d \n",save(pt, size, filenameSave));
 
+    printf("%d \n", sum_file("testBin.bin"));
 }
