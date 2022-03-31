8b2b268915b2a4ebd4b5923ccd9cb645d8c43499
Maxime Delacroix <maxime.dxl2002@gmail.com>
2022-03-31 23:18:29 +0200

    review

--- Semaine4/compFunction.c
+++ Semaine4/compFunction.c
@@ -8,11 +8,9 @@ uint8_t testFunction(uint8_t nb) {
 
 
 int cmp_func(uint8_t func1(uint8_t), uint8_t func2(uint8_t)) {
+    if (func1 == func2) return 1; 
     for (unsigned int i = 0; i<256; i++) {
-        if (func1(i)==func2(i)) {}
-        else if (func1 == func2) {
-            return 1;
-        }
+        if (func1(i)==func2(i)) continue;
         else {
             return 0;
         }
@@ -22,7 +20,7 @@ int cmp_func(uint8_t func1(uint8_t), uint8_t func2(uint8_t)) {
 
 
 int main(int argc, char* argv) {
-    testFunctionPointer(uint8_t) = testFunction;
+    uint_8 testFunctionPointer(uint8_t) = testFunction;
     printf("%d \n", cmp_func(testFunction,testFunction));
     printf("%d \n", (*testFunctionPointer)(125));
 
